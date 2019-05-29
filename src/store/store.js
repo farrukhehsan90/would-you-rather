7e421ccff75e0ebf9c4653ee901c1190710ef6ec
Farrukh Ehsan <farrukhehsan90@gmail.com>
2019-05-30 01:19:00 +0530

    Removed the module throwing error

--- src/store/store.js
+++ src/store/store.js
@@ -5,8 +5,7 @@ import rootReducer from '../reducers/rootReducer';
 const middleware=[thunk];
 
 const store=createStore(rootReducer,{},compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
 ));
 
 export default store;
